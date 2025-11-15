469782f1b4620f43ee3651ef3bbb76b86c2ef723
zhangziqi <renoelis@outlook.com>
2025-11-16 01:22:13 +0800

    从Git追踪中移除被忽略的文件

--- test/Buffer/test/2.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const hash = crypto.createHash('sha256')
-  .update('hello world')
-  .digest(); // 默认返回 Buffer
-
-console.log(JSON.stringify(hash,null,2));            // <Buffer ...>
-console.log(Buffer.isBuffer(hash)); // true
--- test/dayjs/test.js
+++
@@ -1,8 +0,0 @@
-const dayjs = require('dayjs')
-const relativeTime = require('dayjs/plugin/relativeTime')
-const zhCN = require('dayjs/locale/zh-cn')
-
-dayjs.extend(relativeTime)
-dayjs.locale(zhCN)
-
-console.log(dayjs().from(dayjs('2020-01-01')))
